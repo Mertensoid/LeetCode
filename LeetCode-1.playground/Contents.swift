dd1a2924019d5627be50e34dd19dd573afcdee7e
Mertensoid <matsoukm@yandex.ru>
2021-11-15 20:10:50 +0300

    Решил Problem 2!!!

--- LeetCode-1.playground/Contents.swift
+++ LeetCode-1.playground/Contents.swift
@@ -29,160 +29,80 @@ public class ListNode {
       public init() { self.val = 0; self.next = nil; }
       public init(_ val: Int) { self.val = val; self.next = nil; }
       public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
-  }
-
-func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> () {
-    var listNode = l1
-    var count = 0
-    var sum1: Int32 = 0
-    var sum2: Int32 = 0
-    
-    repeat {
-        var result: Int32 = 0
-        if let res = listNode?.val {
-            var i = 0
-            result = Int32(res)
-            while i < count {
-                result = result * 10
-                i += 1
-            }
-        }
-        sum1 = sum1 + result
-        count += 1
-        listNode = listNode?.next
-        print(sum1)
-    } while listNode?.next != nil
-    
-    listNode = l2
-    count = 0
-    
-    repeat {
-        var result: Int32 = 0
-        if let res = listNode?.val {
-            var i = 0
-            result = Int32(res)
-            while i < count {
-                result = result * 10
-                i += 1
-            }
-        }
-        sum2 = sum2 + result
-        
-        count += 1
-        listNode = listNode?.next
-        print(sum2)
-    } while listNode?.next != nil
 }
 
-//    if let res = listNode1?.val {
-//        listNodeArray1.insert(Int32(res), at: 0)
-//    }
-//
-//    while listNode2?.next != nil {
-//        if let res = listNode2?.val {
-//            listNodeArray2.insert(Int32(res), at: 0)
-//        }
-//        listNode2 = listNode2?.next
-//    }
-//
-//    if let res = listNode2?.val {
-//        listNodeArray2.insert(Int32(res), at: 0)
-//    }
-//    print(listNodeArray1)
-//    print(listNodeArray2)
-//    print(makeNum(array: listNodeArray1))
-//    print(makeNum(array: listNodeArray2))
-//
-//    let sum: Int32 = makeNum(array: listNodeArray1) + makeNum(array: listNodeArray2)
-//    var linkedRes: ListNode? = nil
-//    if sum != 0 {
-//
-//        var array: [Int] = []
-//        var res: Int32 = sum
-//        var ost: Int32 = 0
-//
-//
-//        while res != 0 {
-//            ost = res % 10
-//            array.insert(Int(ost), at: 0)
-//            res = res / 10
-//        }
-//
-//        for i in array {
-//            linkedRes = ListNode.init(i, linkedRes)
-//        }
-//
-//    } else {
-//        linkedRes = ListNode.init()
-//    }
-//    return linkedRes
-//}
+func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+    var listNode1 = l1
+    var listNode2 = l2
 
-//ЗДЕСЬ ОШИБКА! Я НЕ ПОНИМАЮ КАКОГО ХРЕНА ЭТО НЕ РАБОТАЕТ
-//func makeNum(array: [Int32]) -> Int32 {
-//    var inArray: [Int32] = array
-//    var sum: Int32 = 0
-//    var res: Int32 = 0
-//    var arrayCount = inArray.count
-//    while arrayCount != 0 {
-//        print(inArray.count)
-//        res = Int32(inArray[0])
-//        var count = 1
-//        while count < arrayCount {
-//            res *= 10
-//            count += 1
-//        }
-//        arrayCount -= 1
-//        sum += res
-//    }
-//    return(sum)
-//}
-
-//func makeList(sum: Int) -> () {
-//    if sum != 0 {
-//        var array: [Int] = []
-//        var res = sum
-//        var ost = 0
-//        var linkedRes: ListNode? = nil
-//
-//        while res != 0 {
-//            ost = res % 10
-//            array.insert(ost, at: 0)
-//            res = res / 10
-//        }
-//
-//        for i in array {
-//            linkedRes = ListNode.init(i, linkedRes)
-//        }
-//        print(array)
-//    } else {
-//
-//    }
+    var value1 = 0
+    var value2 = 0
+    var remem = 0
+    var sum = 0
+    var array: [Int] = []
     
+    repeat {
+        if let valueListNode = listNode1?.val {
+            if listNode1?.next != nil {
+                listNode1 = listNode1?.next
+            } else {
+                listNode1 = ListNode.init()
+            }
+            value1 = valueListNode
+            print("Val1 = \(value1)")
+        }
+        if let valueListNode = listNode2?.val {
+            
+            if listNode2?.next != nil {
+                listNode2 = listNode2?.next
+            } else {
+                listNode2 = ListNode.init()
+            }
+            value2 = valueListNode
+            print("Val2 = \(value2)")
+        }
+        sum = value1 + value2 + remem
+        if sum > 9 {
+            remem = sum / 10
+            sum = sum % 10
+            print(sum)
+        } else {
+            remem = 0
+        }
+        
+        array.insert(sum, at: 0)
+        
+        if listNode1?.next == nil && listNode2?.next == nil && listNode1?.val == 0 && listNode2?.val == 0 {
+            break
+        }
+        
+    } while true
     
+    if remem == 1 {
+        array.insert(remem, at: 0)
+    }
+    print(array)
+    var linkedRes: ListNode? = nil
 
-    //return linkedRes
-    
-//    var listNodeArray2: [Int] = []
-//
-//    while linkedRes?.next != nil {
-//        if let res = linkedRes?.val {
-//            listNodeArray2.insert(res, at: 0)
-//        }
-//        linkedRes = linkedRes?.next
-//    }
-//
-//    if let res = linkedRes?.val {
-//        listNodeArray2.insert(res, at: 0)
-//    }
-//
-//    print(listNodeArray2)
-//
-//    return array
+        for i in array {
+            linkedRes = ListNode.init(i, linkedRes)
+        }
 
+    return linkedRes
+}
 
 let ln1 = ListNode.init(9)
-let ln2 = ListNode.init(1, ListNode.init(9, ListNode.init(9, ListNode.init(9, ListNode.init(9,ListNode.init(9, ListNode.init(9, ListNode.init(9, ListNode.init(9, ListNode.init(9, ListNode.init(9)))))))))))
+let ln2 = ListNode.init(1, ListNode.init(
+                        9, ListNode.init(
+                        9, ListNode.init(
+                        9, ListNode.init(
+                        9, ListNode.init(
+                        9, ListNode.init(
+                        9, ListNode.init(
+                        9, ListNode.init(
+                        9, ListNode.init(
+                        9, ListNode.init(
+                        9)))))))))))
 
 addTwoNumbers(ln1, ln2)
 
