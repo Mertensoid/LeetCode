321ba9cebfcfa6b207e7391a112da7d802b10c17
Mertensoid <matsoukm@yandex.ru>
2021-12-04 21:50:35 +0300

    Started Problem 28

---
+++ Problem 28.playground/Contents.swift
@@ -0,0 +1,14 @@
+
+func strStr(_ haystack: String, _ needle: String) -> Int {
+    var
+    for _ in 0...haystack.count-needle.count-1 {
+        var subString: String = ""
+        for _ in 0...needle.count-1 {
+            subString.append(needle.remove(at: 0))
+        }
+        var subString = haystack.
+        return 0
+    }
+}
+
+print(strStr("aaaaa", "bba"))
